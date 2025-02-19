7cd1c96f3c2160390aa8729430ddf82202ab2ad6
Ledon <ledon.jhonryan05@gmail.com>
2025-02-19 23:28:00 +0800

    screens XD

---
+++ app/src/main/java/com/android/noteit/SplashscreenActivity.kt
@@ -0,0 +1,16 @@
+package com.android.noteit
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashscreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+
+        supportActionBar?.hide()
+    }
+}
